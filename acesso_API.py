4ed256b23cbcd8ad1838ddd3fefd986e0f343aa3
Alexandre Victor <alexandrevct@hotmail.com>
2018-08-21 17:30:56 -0300

    arquivos atualizados

--- acesso_API.py
+++ acesso_API.py
@@ -2,6 +2,8 @@ __author__='AlVictor'
 
 import requests
 import simplejson as json
+from grava_arquivos  import GravaArquivos
+
 class AcessoAPI:
 
 
@@ -10,9 +12,12 @@ class AcessoAPI:
                     'authorization': "Basic d2FBcGk6Y2MxYzg3NjQtNTc3NS00OGRhLWE4N2UtNGY1M2QwZDIyMDQ2",
                     'cache-control': "no-cache"
                     }
-
-        def __init__(self, querystring):
+        pageNumber = 1
+        dados_py = []
+    
+        def __init__(self, querystring,nomearquivo):
             self.querystring = querystring
+            self.nomearquivo = nomearquivo
                                         
         def conecta(self):
             response  = requests.request("GET", self.__class__.url, headers=self.__class__.cabecalho)
@@ -21,9 +26,21 @@ class AcessoAPI:
             else:
              return print(response.text)
         def recuperaregistros(self):
-            response  = requests.request("GET", self.__class__.url, headers=self.__class__.cabecalho, params=self.querystring)
-            dados_json = json.loads(response.content)
-            #Captura o total de pags
-            #return response.headers['X-Pagination-TotalPages']
-            return dados_json
-teste
+            response  = requests.request("GET", self.__class__.url, headers=self.__class__.cabecalho, params=self.querystring+str(self.__class__.pageNumber)+"}")
+            totalpags = int(response.headers['X-Pagination-TotalPages'])
+            
+            print("Total de Paginas:",totalpags)
+            if totalpags > 0:
+                while totalpags >= self.__class__.pageNumber  :
+                         response  = requests.request("GET", self.__class__.url, headers=self.__class__.cabecalho, params=self.querystring+str(self.__class__.pageNumber)+"}")            
+                         TotalLinhas = int(response.headers['x-pagination-totalresults'])
+                         print("Total Linhas",TotalLinhas)
+                         print("Pagina Atual: ",self.__class__.pageNumber,"/",totalpags)
+                         print(self.querystring+str(self.__class__.pageNumber)+"}")
+                         self.__class__.dados_py += json.loads(response.text)
+                         self.__class__.pageNumber += 1
+            else:
+                return print("Não existem dados")
+            gravar = GravaArquivos(self.nomearquivo,self.__class__.dados_py)
+            gravar.save_json()
+            return None
---
+++ grava_arquivos.py
@@ -0,0 +1,25 @@
+import csv
+try:
+    import json
+except ImportError:
+    import simplejson as json
+    
+class GravaArquivos:
+
+        def __init__(self, filename, file):
+            self.filename = filename
+            self.file = file
+    
+        def save_json(self):
+            with open(self.filename+'.json', 'w') as f:
+                data_json = json.dumps(self.file)
+                f.write(str(data_json))
+                f.close()
+        def save_csv(self):
+            with open(self.filename+'.csv', 'w', newline='') as f:
+                data_csv =  csv.writer(f, delimiter=' ', quotechar=';', quoting=csv.QUOTE_MINIMAL)
+                data_csv.writerow(self.file)
+
+
+
+    
